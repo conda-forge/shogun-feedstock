dca63f33d4a64eb62403adad0fe2535cf66c31bc
Dougal J. Sutherland <dougal@gmail.com>
2017-11-29 11:23:19 +0000

    get_tarball: don't verify ssl cert

--- recipe/get_tarball.py
+++ recipe/get_tarball.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import argparse
-from glob import glob
 import hashlib
-import os
 import io
-from six.moves.urllib.request import urlretrieve
+import os
 import shutil
 import tarfile
 import tempfile
+from glob import glob
+
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url')
@@ -20,12 +21,17 @@ args = parser.parse_args()
 
 # should be possible to do this in one pass, but requires "tee"ing the file to
 # both GzipFile and hashlib, so whatever.
+chunksize = 32 * 2**20
 
-tar_fn, _ = urlretrieve(args.url)
+tar_fn = 'shogun-gpl.tar.gz'
+response = requests.get(args.url, verify=False, stream=True)
+response.raise_for_status()
+with open(tar_fn, 'wb') as f:
+    for block in response.iter_content(chunksize):
+        f.write(block)
 
 try:
     digest = hashlib.new(args.checksum_type)
-    chunksize = 32 * 2**20
     with io.open(tar_fn, 'rb') as f:
         while True:
             x = f.read(chunksize)
